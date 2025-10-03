15a579419b10d78166528d3889dbaf41176ffe54
Tony1234765 <perezflorescarlosantonio@gmail.com>
2025-10-03 12:30:01 -0600

    tailwind funcionando

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-
+import "./index.css";
 const theme = createTheme({
   palette: {
     mode: "light",
