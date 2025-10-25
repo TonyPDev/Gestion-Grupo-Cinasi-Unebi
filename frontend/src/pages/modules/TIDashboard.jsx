2b0de2fa9f8b4171ee8924636e50c8306ecb474e
Tony1234765 <perezflorescarlosantonio@gmail.com>
2025-10-24 20:59:37 -0600

    Cambié el acpu de TI a administración con sus respectivos permisos

--- frontend/src/pages/modules/TIDashboard.jsx
+++ frontend/src/pages/modules/TIDashboard.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { KeySquare, Settings } from "lucide-react";
-import { KeySquare, Settings } from "lucide-react";
 
 function TIDashboard() {
   return (
